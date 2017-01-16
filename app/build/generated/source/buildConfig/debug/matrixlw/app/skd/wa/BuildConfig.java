60801b44999273bf8c0ec4ed931edc6c601f9b61
sapandang <skdang1993@gmail.com>
2017-01-16 21:35:56 +0530

    added all the files

---
+++ app/build/generated/source/buildConfig/debug/matrixlw/app/skd/wa/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package matrixlw.app.skd.wa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "matrixlw.app.skd.matrixlivewallpaper";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
